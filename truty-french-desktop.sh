60cbc18dbc30bc7d8e0777e0b3f8dbd5ebf32f64
Kimpe Andy <andykimpe@gmail.com>
2014-06-16 16:53:41 +0200

    Update truty-french-desktop.sh

--- truty-french-desktop.sh
+++ truty-french-desktop.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 apt-get update
 apt-get -y dist-upgrade
-apt-get -y install ubuntu-desktop language-pack-fr language-pack-fr-base language-pack-gnome-fr language-pack-gnome-fr-base gnome-session-fallback
-useradd -d /home/Desktop -s /bin/bash -m Desktop
-cp -R /etc/skel/* /home/Desktop
-echo "Desktop:Desktop" | chpasswd
-add-apt-repository -y ppa:x2go/stable
-apt-get update
-apt-get install -y x2goserver x2goserver-xsession
-apt-get clean
 echo "OK"

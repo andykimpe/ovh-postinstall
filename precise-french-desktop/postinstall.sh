03cfe7daa21cd362cf942f84af05efe1a28061cc
Kimpe Andy <andykimpe@gmail.com>
2014-06-16 17:51:15 +0200

    Update postinstall.sh

--- precise-french-desktop/postinstall.sh
+++ precise-french-desktop/postinstall.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+rm -rf /etc/apt/sources.list /etc/apt/sources.list.d/
+mkdir /etc/apt/sources.list.d/
+wget https://github.com/andykimpe/ovh-postinstall/raw/master/precise-french-desktop/sources.list -P /etc/apt/
+apt-get update
+apt-get -y dist-upgrade
+apt-get -y autoremove
+apt-get -y install ubuntu-desktop gnome-session-fallback language-pack-fr language-pack-fr-base language-pack-gnome-fr language-pack-gnome-fr-base ubuntu-restricted-extras
+echo "OK"

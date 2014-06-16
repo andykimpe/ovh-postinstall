c1409731522d041ac7eb34be2032d2fce5728361
Kimpe Andy <andykimpe@gmail.com>
2014-06-16 21:12:06 +0200

    Update postinstall.sh

--- precise-french-desktop/postinstall.sh
+++ precise-french-desktop/postinstall.sh
@@ -11,10 +11,11 @@ apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 99B2B9B3274A7DA0
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 5A9A06AEF9CB8DB0
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 7CDE3A860A53F9FD
-dpkg --add-architecture i386
 apt-get update
 apt-get -y dist-upgrade
 apt-get -y autoremove
 apt-get -y install ubuntu-desktop gnome-session-fallback language-pack-fr language-pack-fr-base language-pack-gnome-fr language-pack-gnome-fr-base ubuntu-restricted-extras chromium-browser
+dpkg --add-architecture i386
+apt-get update
 apt-get -y install freshplayerplugin skype wine1.7 playonlinux openjdk-7-jre icedtea-7-plugin vlc
 echo "OK"

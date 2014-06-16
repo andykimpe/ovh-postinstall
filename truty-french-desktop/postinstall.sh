d3e1d590f74a3926611dcb322383ad61cd9e224a
Kimpe Andy <andykimpe@gmail.com>
2014-06-16 17:56:11 +0200

    Update postinstall.sh

--- truty-french-desktop/postinstall.sh
+++ truty-french-desktop/postinstall.sh
@@ -4,7 +4,7 @@ rm -rf /etc/apt/sources.list /etc/apt/sources.list.d/
 mkdir /etc/apt/sources.list.d/
 wget https://github.com/andykimpe/ovh-postinstall/raw/master/truty-french-desktop/sources.list -P /etc/apt/
 apt-get update
-apt-get -y --force-yes dist-upgrade
+apt-get -y --force-yes -f dist-upgrade
 apt-get -y autoremove
 apt-get -y install ubuntu-desktop gnome-session-fallback language-pack-fr language-pack-fr-base language-pack-gnome-fr language-pack-gnome-fr-base ubuntu-restricted-extras
 echo "OK"

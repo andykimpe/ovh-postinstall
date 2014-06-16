b05939fae452562ab88549625734c9886b8c2e1e
Kimpe Andy <andykimpe@gmail.com>
2014-06-16 19:25:30 +0200

    Update postinstall.sh

--- truty-french-desktop/postinstall.sh
+++ truty-french-desktop/postinstall.sh
@@ -5,6 +5,7 @@ mkdir /etc/apt/sources.list.d/
 wget https://github.com/andykimpe/ovh-postinstall/raw/master/truty-french-desktop/sources.list -P /etc/apt/
 wget https://github.com/andykimpe/ovh-postinstall/raw/master/truty-french-desktop/andykimpe-freshplayerplugin-daily-trusty.list -P /etc/apt/sources.list.d/
 wget https://github.com/andykimpe/ovh-postinstall/raw/master/truty-french-desktop/andykimpe-skype-trusty.list -P /etc/apt/sources.list.d/
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 99B2B9B3274A7DA0
 apt-get update
 apt-get -y --force-yes -f dist-upgrade
 apt-get -y autoremove

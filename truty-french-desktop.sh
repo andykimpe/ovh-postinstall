836c0e5e3a0864f73cde4cff7ad9f0f7d7c09501
Kimpe Andy <andykimpe@gmail.com>
2014-06-16 16:06:22 +0200

    Update truty-french-desktop.sh

--- truty-french-desktop.sh
+++ truty-french-desktop.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-adduser Desktop
-mkdir /home/Desktop
-cp /et/skel/* /home/Desktop
 apt-get update
 apt-get -y dist-upgrade
 apt-get -y install ubuntu-desktop language-pack-fr language-pack-fr-base language-pack-gnome-fr language-pack-gnome-fr-base
+useradd -d /home/Desktop -s /bin/bash -m Desktop
 echo "Desktop:Desktop" | chpasswd
 add-apt-repository -y ppa:x2go/stable
 apt-get update

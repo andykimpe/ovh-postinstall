f6726adb7747667503dabedc7eaa5c8fea707618
Kimpe Andy <andykimpe@gmail.com>
2014-06-16 17:20:36 +0200

    Update truty-french-desktop.sh

--- truty-french-desktop.sh
+++ truty-french-desktop.sh
@@ -3,6 +3,6 @@ apt-get update
 apt-get -y dist-upgrade
 apt-get -y autoremove
 apt-get -y install update-manager-core
-do-release-upgrade -d -q
+do-release-upgrade -d -f DistUpgradeViewNonInteractive
 apt-get -y install ubuntu-desktop gnome-session-fallback language-pack-fr language-pack-fr-base language-pack-gnome-fr language-pack-gnome-fr-base
 echo "OK"

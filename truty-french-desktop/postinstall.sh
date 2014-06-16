8d0cc011c9cae98912bd140149de0fa92b48d339
Kimpe Andy <andykimpe@gmail.com>
2014-06-16 19:09:03 +0200

    Update postinstall.sh

--- truty-french-desktop/postinstall.sh
+++ truty-french-desktop/postinstall.sh
@@ -3,6 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 rm -rf /etc/apt/sources.list /etc/apt/sources.list.d/
 mkdir /etc/apt/sources.list.d/
 wget https://github.com/andykimpe/ovh-postinstall/raw/master/truty-french-desktop/sources.list -P /etc/apt/
+wget https://github.com/andykimpe/ovh-postinstall/raw/master/truty-french-desktop/andykimpe-freshplayerplugin-daily-trusty.list -P /etc/apt/sources.list.d/
 apt-get update
 apt-get -y --force-yes -f dist-upgrade
 apt-get -y autoremove

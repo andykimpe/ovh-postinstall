5d4ff9d0d6089ea2e87ffbf2a9c00e4d16336d7a
Kimpe Andy <andykimpe@gmail.com>
2014-06-16 20:57:55 +0200

    Update postinstall.sh

--- precise-french-desktop/postinstall.sh
+++ precise-french-desktop/postinstall.sh
@@ -6,9 +6,11 @@ wget https://github.com/andykimpe/ovh-postinstall/raw/master/precise-french-desk
 wget https://github.com/andykimpe/ovh-postinstall/raw/master/precise-french-desktop/andykimpe-freshplayerplugin-daily-precise.list -P /etc/apt/sources.list.d/
 wget https://github.com/andykimpe/ovh-postinstall/raw/master/precise-french-desktop/andykimpe-skype-precise.list -P /etc/apt/sources.list.d/
 wget https://github.com/andykimpe/ovh-postinstall/raw/master/precise-french-desktop/ubuntu-wine-ppa-precise.list -P /etc/apt/sources.list.d/
+wget https://github.com/andykimpe/ovh-postinstall/raw/master/precise-french-desktop/x2go-stable-precise.list -P /etc/apt/sources.list.d/
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 99B2B9B3274A7DA0
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 5A9A06AEF9CB8DB0
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 7CDE3A860A53F9FD
 dpkg --add-architecture i386
 apt-get update
 apt-get -y dist-upgrade

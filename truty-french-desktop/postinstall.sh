749cb3c4117c72c81e7700ee0b96d332ec5f476f
Kimpe Andy <andykimpe@gmail.com>
2014-06-16 19:19:16 +0200

    Update postinstall.sh

--- truty-french-desktop/postinstall.sh
+++ truty-french-desktop/postinstall.sh
@@ -7,5 +7,5 @@ wget https://github.com/andykimpe/ovh-postinstall/raw/master/truty-french-deskto
 apt-get update
 apt-get -y --force-yes -f dist-upgrade
 apt-get -y autoremove
-apt-get -y install ubuntu-desktop gnome-session-fallback language-pack-fr language-pack-fr-base language-pack-gnome-fr language-pack-gnome-fr-base ubuntu-restricted-extras
+apt-get -y --force-yes -f install ubuntu-desktop gnome-session-fallback language-pack-fr language-pack-fr-base language-pack-gnome-fr language-pack-gnome-fr-base ubuntu-restricted-extras freshplayerplugin
 echo "OK"

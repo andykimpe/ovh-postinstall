50fc8ac1e86c86357fb2cfabba24c39e0c1beaf7
Kimpe Andy <andykimpe@gmail.com>
2014-06-18 19:18:33 +0200

    Update postinstall.sh

--- centos-6-solusvm-master/postinstall.sh
+++ centos-6-solusvm-master/postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #please beta dot use
 rm -f /etc/yum.repos.d/CentOS-Base.repo
-wget https://github.com/andykimpe/ovh-postinstall/raw/master/centos-6/CentOS-Base.repo -P /etc/yum.repos.d/
+wget --no-check-certificate https://github.com/andykimpe/ovh-postinstall/raw/master/centos-6/CentOS-Base.repo -P /etc/yum.repos.d/
 yum -y update
 mkdir -p /usr/local/solusvm/tmp/
 yum -y install glibc glibc-devel glibc-devel.i686 perl php php-cli php-common
@@ -9,7 +9,7 @@ wget http://files.eu.fr.soluslabs.com/solusvm/loaders/ioncube_loaders_lin_x86_64
 wget http://files.eu.fr.soluslabs.com/solusvm/loaders/php.ini.64.6 -O /etc/php.ini
 userdel solusvm
 adduser -d /usr/local/solusvm -s /sbin/nologin solusvm
-wget https://github.com/andykimpe/ovh-postinstall/raw/master/centos-6-solusvm-master/soluslabs.repo -O /etc/yum.repos.d/soluslabs.repo
+wget --no-check-certificate https://github.com/andykimpe/ovh-postinstall/raw/master/centos-6-solusvm-master/soluslabs.repo -O /etc/yum.repos.d/soluslabs.repo
 yum clean all
 tar vxzf /usr/local/solusvm/tmp/ioncube_loaders.tar.gz -C /etc/
 wget http://files.eu.fr.soluslabs.com/solusvm/installer/v3/installscripts.tar.gz -O /usr/local/solusvm/tmp/installscripts.tar.gz

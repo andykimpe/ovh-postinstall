74b58f4d7099369d9c8918e4ae58d0cafbe76098
Kimpe Andy <andykimpe@gmail.com>
2014-06-18 05:54:02 +0200

    Update postinstall.sh

--- centos-6/postinstall.sh
+++ centos-6/postinstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rm -f /etc/yum.repos.d/CentOS-Base.repo
+wget https://github.com/andykimpe/ovh-postinstall/raw/master/centos-6/CentOS-Base.repo -P /etc/yum.repos.d/
+yum -y update
 echo "OK"
 exit

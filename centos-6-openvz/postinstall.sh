b3825de61d5c063976b3c5617bf1c3a90ec45b27
Kimpe Andy <andykimpe@gmail.com>
2014-06-20 01:11:49 +0200

    Update and rename centos-6-solusvm-master/postinstall.sh to centos-6-openvz/postinstall.sh

---
+++ centos-6-openvz/postinstall.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+rm -f /etc/yum.repos.d/CentOS-Base.repo
+wget --no-check-certificate https://github.com/andykimpe/ovh-postinstall/raw/master/centos-6/CentOS-Base.repo -P /etc/yum.repos.d/
+wget -P /etc/yum.repos.d/ http://ftp.openvz.org/openvz.repo
+rpm --import http://ftp.openvz.org/RPM-GPG-Key-OpenVZ
+yum -y update
+yum -y install vzkernel
+cat > /etc/sysctl.conf <<EOF
+# On Hardware Node we generally need
+# packet forwarding enabled and proxy arp disabled
+net.ipv4.ip_forward = 1
+net.ipv6.conf.default.forwarding = 1
+net.ipv6.conf.all.forwarding = 1
+net.ipv4.conf.default.proxy_arp = 0
+
+# Enables source route verification
+net.ipv4.conf.all.rp_filter = 1
+
+# Enables the magic-sysrq key
+kernel.sysrq = 1
+
+# We do not want all our interfaces to send redirects
+net.ipv4.conf.default.send_redirects = 1
+net.ipv4.conf.all.send_redirects = 0
+EOF
+echo "SELINUX=disabled" > /etc/sysconfig/selinux
+yum -y install vzctl vzquota ploop
+yum -y remove kernel
+kernel1=$(find /boot -name  vmlinuz*)
+kernel2=$(find /boot -name  initramfs*)
+chmod 777 /boot/grub/grub.conf
+cat > /boot/grub/grub.conf <<EOF
+default=0
+timeout=5
+
+	title linux solusvm-master-vz_64
+	kernel $kernel1 root=/dev/sda1  ro
+	root (hd0,0)
+	initrd $kernel2
+EOF
+echo "OK"
+exit

27f571fcb4c348711d8ca5ad81d5c3ace3065202
Kimpe Andy <andykimpe@gmail.com>
2016-01-10 14:00:08 +0100

    Update postinstall.sh

--- CentOS/6/x2go-gnome-desktop/postinstall.sh
+++ CentOS/6/x2go-gnome-desktop/postinstall.sh
@@ -10,8 +10,14 @@ yum -y groupinstall "Graphics Creation Tools"
 yum -y groupinstall "Legacy X Window System compatibility"
 wget -O /etc/yum.repos.d/x2go.repo http://packages.x2go.org/epel/x2go.repo
 yum -y install x2goserver x2goserver-xsession x2goclient
+yum -y install wine
+yum -y install http://rpm.playonlinux.com/fedora/yum/base/playonlinux-yum-4-1.noarch.rpm
+yum -y install playonlinux
 service sshd restart
 adduser desktop
 echo "desktop:desktop" | chpasswd
+echo "desktop    ALL=(ALL)       ALL" > /etc/sudoers.d/desktop
+chmod 777 /etc/sudoers.d/desktop
+sed -i 's/#includedir /etc/sudoers.d/includedir /etc/sudoers.d/g' "/etc/sudoers"
 echo "OK"
 exit

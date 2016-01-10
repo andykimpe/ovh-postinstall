99f128757a3cb2026cc264b936cf9cd8fa408db9
Kimpe Andy <andykimpe@gmail.com>
2016-01-10 12:45:39 +0100

    Create postinstall.sh

---
+++ CentOS/6/x2go-gnome-desktop/postinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+yum -y install epel-release wget nano
+yum -y update
+yum -y groupinstall "Desktop" "Desktop Platform" "X Window System" "Fonts"
+yum -y groupinstall "Graphical Administration Tools"
+yum -y groupinstall "Internet Browser"
+yum -y groupinstall "General Purpose Desktop"
+yum -y groupinstall "Office Suite and Productivity"
+yum -y groupinstall "Graphics Creation Tools"
+yum -y groupinstall "Legacy X Window System compatibility"
+wget -O /etc/yum.repos.d/x2go.repo http://packages.x2go.org/epel/x2go.repo
+yum -y install x2goserver x2goserver-xsession x2goclient
+service sshd restart
+adduser desktop
+echo "desktop:desktop" | chpasswd
+reboot
+exit

04f48354d7ce05b87104c50c3d8ff8de169365c7
Kimpe Andy <andykimpe@gmail.com>
2016-01-10 11:40:52 +0100

    Delete postinstall.sh

--- centos-6/postinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -f /etc/yum.repos.d/CentOS-Base.repo
-wget https://github.com/andykimpe/ovh-postinstall/raw/master/centos-6/CentOS-Base.repo -P /etc/yum.repos.d/
-yum -y update
-echo "OK"
-exit

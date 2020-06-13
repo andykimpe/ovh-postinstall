aa552321592ef395139ec24f09f8150126f2d8fd
Kimpe Andy <andykimpe@gmail.com>
2020-06-13 23:29:45 +0200

    Update postinstall.sh

--- kimsufi/centos6-openvz/postinstall.sh
+++ kimsufi/centos6-openvz/postinstall.sh
@@ -4,21 +4,7 @@ wget -P /etc/yum.repos.d/ https://github.com/andykimpe/ovh-postinstall/raw/maste
 rpm --import https://github.com/andykimpe/ovh-postinstall/raw/master/kimsufi/centos6-openvz/RPM-GPG-Key-OpenVZ
 yum -y update
 yum -y install vzkernel
-cat > /etc/sysctl.conf <<EOF
-# On Hardware Node we generally need
-# packet forwarding enabled and proxy arp disabled
-net.ipv4.ip_forward = 1
-net.ipv6.conf.default.forwarding = 1
-net.ipv6.conf.all.forwarding = 1
-net.ipv4.conf.default.proxy_arp = 0
-# Enables source route verification
-net.ipv4.conf.all.rp_filter = 1
-# Enables the magic-sysrq key
-kernel.sysrq = 1
-# We do not want all our interfaces to send redirects
-net.ipv4.conf.default.send_redirects = 1
-net.ipv4.conf.all.send_redirects = 0
-EOF
+wget -O /etc/sysctl.conf https://github.com/andykimpe/ovh-postinstall/raw/master/kimsufi/centos6-openvz/sysctl.conf
 echo "SELINUX=disabled" > /etc/sysconfig/selinux
 yum -y install vzctl vzquota ploop
 yum -y remove kernel
